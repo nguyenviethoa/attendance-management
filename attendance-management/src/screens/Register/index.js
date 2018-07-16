3038a420b6f6e957a3d2a88d42e70125333e33ff
nguyenviethoa <nguyenviethoa@gmail.com>
2018-07-16 16:57:54 +0700

    add signup mutation and error hhandling in signup screen

--- attendance-management/src/screens/Register/index.js
+++ attendance-management/src/screens/Register/index.js
@@ -1,23 +1,45 @@
 import React from 'react';
-import { Button, Input, Container, Header } from 'semantic-ui-react';
+import { Message, Button, Input, Container, Header } from 'semantic-ui-react';
 
 import { RegisterUser } from './mutations';
 
 class Register extends React.Component {
   state = {
     username: '',
+    usernameError: '',
     email: '',
+    emailError: '',
     password: '',
+    passwordError: '',
     companyId: 2,
     role: 'NORMAL'
   };
 
   onSubmit = async (createUser) => {
-    const response = await createUser({
-      variables: this.state,
+    this.setState({
+      usernameError: '',
+      emailError: '',
+      passwordError: '',
     });
+    console.log('start register');
+    const { username, password, email, companyId, role } = this.state;
 
-    console.log(response);
+    try {
+      const response = await createUser({
+        variables: { username, password, email, companyId, role },
+      });
+      console.log('response', response);
+      if (response.data.createUser.passwordError) {
+        this.setState({passwordError: response.data.createUser.passwordError })
+      }
+
+      if (response.data.createUser.ok) {
+        this.props.history.push('/');
+      }
+    }
+    catch(error) {
+      console.log('error', error);
+    };
   };
 
   onChange = e => {
@@ -27,7 +49,20 @@ class Register extends React.Component {
   };
 
   render() {
-    const { username, email, password } = this.state;
+    const { username, email, password, usernameError, emailError, passwordError } = this.state;
+    const errorList = [];
+
+    if (usernameError) {
+      errorList.push(usernameError);
+    }
+
+    if (emailError) {
+      errorList.push(emailError);
+    }
+
+    if (passwordError) {
+      errorList.push(passwordError);
+    }
 
     return (
       <RegisterUser>
@@ -37,6 +72,7 @@ class Register extends React.Component {
               <Container text>
                 <Header as="h2">Register</Header>
                 <Input
+                  error={!!usernameError}
                   name="username"
                   onChange={this.onChange}
                   value={username}
@@ -45,6 +81,7 @@ class Register extends React.Component {
                 />
                 <Input name="email" onChange={this.onChange} value={email} placeholder="Email" fluid />
                 <Input
+                  error={!!passwordError}
                   name="password"
                   onChange={this.onChange}
                   value={password}
@@ -53,6 +90,10 @@ class Register extends React.Component {
                   fluid
                 />
                 <Button onClick={() => this.onSubmit(createUser)}>Submit</Button>
+
+                {usernameError || emailError || passwordError ? (
+                  <Message error header="There was some errors with your submission" list={errorList} />
+                ) : null}
               </Container>
             )
           }
--- attendance-management/src/screens/Register/mutations.js
+++ attendance-management/src/screens/Register/mutations.js
@@ -3,16 +3,23 @@ import { Mutation } from "react-apollo";
 import React from "react";
 
 const registerMutation = gql`
-  mutation($username: String!, $email: String!, $password: String!) {
-    createUser(username: $username, email: $email, password: $password, companyId: $companyId, role: $role)
+  mutation($username: String!, $email: String!, $password: String!, $companyId: Int!, $role: String) {
+    createUser(username: $username, email: $email, password: $password, companyId: $companyId, role: $role) {
+      ok
+      user{
+        username
+      }
+      usernameError
+      passwordError
+      emailError
+    }
   }
 `;
 
-export const RegisterUser = ({children})=>(
+export const RegisterUser = ({ children })=>(
   <Mutation mutation={registerMutation} >
     {
-      (createUser, loading)=>{
-        
+      (createUser, loading) => {
         return children(createUser,loading);
       }
     }
